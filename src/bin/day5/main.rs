cd476794eb5074ce20ecac06f94d0cdb19508283
Guillaume Charmetant <cguille.dev@gmail.com>
2021-12-05 20:14:32 +0100

    Add day 5 part 2

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("{}", part1(input));
+    println!("{}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
@@ -28,28 +29,35 @@ fn part1(input: &str) -> usize {
     points.iter().filter(|(_point, count)| count >= &&2).count()
 }
 
+fn part2(input: &str) -> usize {
+    let mut points = HashMap::new();
+
+    input
+        .lines()
+        .map(|line| {
+            line.parse::<Segment>()
+                .expect(&format!("Invalid segment: {:?}", line))
+        })
+        .for_each(|segment| {
+            segment.iter().for_each(|p| {
+                let count = points.entry(p).or_insert(0);
+                *count += 1;
+            })
+        });
+
+    points.iter().filter(|(_point, count)| count >= &&2).count()
+}
+
 #[derive(Debug, PartialEq)]
 struct Segment(Point, Point);
 
 impl Segment {
     fn iter(&self) -> SegmentIter {
-        let (direction, val0, val1) = if self.0.x == self.1.x {
-            (SegmentIterDirection::Vertical, self.0.y, self.1.y)
-        } else {
-            (SegmentIterDirection::Horizontal, self.0.x, self.1.x)
-        };
-
-        let (start, end) = if val0 < val1 {
-            (self.0, self.1)
-        } else {
-            (self.1, self.0)
-        };
-
         SegmentIter {
-            current: start,
-            direction,
-            end,
-            end_reached: false,
+            started: false,
+            current: self.0,
+            x_delta: Delta::from(self.0.x, self.1.x),
+            y_delta: Delta::from(self.0.y, self.1.y),
         }
     }
 
@@ -59,53 +67,89 @@ impl Segment {
 }
 
 #[derive(Debug)]
-enum SegmentIterDirection {
-    Horizontal,
-    Vertical,
+enum Delta {
+    Positive(u32),
+    Negative(u32),
+    None,
+}
+
+impl Delta {
+    fn from(start: u32, end: u32) -> Self {
+        if start == end {
+            Delta::None
+        } else if start < end {
+            Delta::Positive(end - start)
+        } else {
+            Delta::Negative(start - end)
+        }
+    }
+
+    fn decrease(&mut self) {
+        match self {
+            Self::Positive(ref mut n) if *n > 0 => *n -= 1,
+            Self::Negative(ref mut n) if *n > 0 => *n -= 1,
+            _ => (),
+        };
+    }
+
+    fn is_exhausted(&self) -> bool {
+        match self {
+            Self::None => true,
+            Self::Positive(0) => true,
+            Self::Negative(0) => true,
+            _ => false,
+        }
+    }
 }
 
 #[derive(Debug)]
 struct SegmentIter {
+    started: bool,
     current: Point,
-    direction: SegmentIterDirection,
-    end: Point,
-    end_reached: bool,
+    x_delta: Delta,
+    y_delta: Delta,
 }
 
 impl Iterator for SegmentIter {
     type Item = Point;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.end_reached {
+        if !self.started {
+            self.started = true;
+
+            return Some(self.current);
+        }
+
+        if self.x_delta.is_exhausted() && self.y_delta.is_exhausted() {
             return None;
         }
 
-        let item = self.current;
-
-        match self.direction {
-            SegmentIterDirection::Horizontal => {
-                self.current = Point {
-                    x: self.current.x + 1,
-                    y: self.current.y,
-                };
-
-                if self.current.x > self.end.x {
-                    self.end_reached = true;
+        self.current = Point {
+            x: match self.x_delta {
+                Delta::Positive(_) => {
+                    self.x_delta.decrease();
+                    self.current.x + 1
                 }
-            }
-            SegmentIterDirection::Vertical => {
-                self.current = Point {
-                    x: self.current.x,
-                    y: self.current.y + 1,
-                };
-
-                if self.current.y > self.end.y {
-                    self.end_reached = true;
+                Delta::Negative(_) => {
+                    self.x_delta.decrease();
+                    self.current.x - 1
                 }
-            }
-        }
+                Delta::None => self.current.x,
+            },
+            y: match self.y_delta {
+                Delta::Positive(_) => {
+                    self.y_delta.decrease();
+                    self.current.y + 1
+                }
+                Delta::Negative(_) => {
+                    self.y_delta.decrease();
+                    self.current.y - 1
+                }
+                Delta::None => self.current.y,
+            },
+        };
 
-        Some(item)
+        Some(self.current)
     }
 }
 
@@ -210,6 +254,11 @@ mod day1_tests {
     }
 
     #[test]
+    fn part2_example() {
+        assert_eq!(part2(EXAMPLE), 12);
+    }
+
+    #[test]
     fn parse_point() {
         assert_eq!("3,4".parse(), Ok(Point { x: 3, y: 4 }));
 
@@ -265,14 +314,40 @@ mod day1_tests {
         let segment = Segment(Point { x: 2, y: 9 }, Point { x: 2, y: 5 });
         let mut it = segment.iter();
 
-        assert_eq!(it.next(), Some(Point { x: 2, y: 5 }));
-        assert_eq!(it.next(), Some(Point { x: 2, y: 6 }));
-        assert_eq!(it.next(), Some(Point { x: 2, y: 7 }));
-        assert_eq!(it.next(), Some(Point { x: 2, y: 8 }));
         assert_eq!(it.next(), Some(Point { x: 2, y: 9 }));
+        assert_eq!(it.next(), Some(Point { x: 2, y: 8 }));
+        assert_eq!(it.next(), Some(Point { x: 2, y: 7 }));
+        assert_eq!(it.next(), Some(Point { x: 2, y: 6 }));
+        assert_eq!(it.next(), Some(Point { x: 2, y: 5 }));
         assert_eq!(it.next(), None);
         assert_eq!(it.next(), None);
 
         assert_eq!(segment.iter().count(), 5);
+    }
+
+    #[test]
+    fn test_delta() {
+        assert!(matches!(Delta::from(0, 3), Delta::Positive(3)));
+        assert!(matches!(Delta::from(5, 9), Delta::Positive(4)));
+        assert!(matches!(Delta::from(9, 5), Delta::Negative(4)));
+        assert!(matches!(Delta::from(5, 5), Delta::None));
+
+        assert!(Delta::None.is_exhausted());
+        assert!(Delta::Positive(0).is_exhausted());
+        assert!(Delta::Negative(0).is_exhausted());
+
+        assert!(!Delta::Positive(1).is_exhausted());
+        assert!(!Delta::Negative(1).is_exhausted());
+
+        let mut delta = Delta::Positive(2);
+        assert!(!delta.is_exhausted());
+
+        delta.decrease();
+        assert!(matches!(delta, Delta::Positive(1)));
+        assert!(!delta.is_exhausted());
+
+        delta.decrease();
+        assert!(matches!(delta, Delta::Positive(0)));
+        assert!(delta.is_exhausted());
     }
 }
