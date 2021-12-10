ef68c3753d612e87dadb286567c341898bb127e7
Guillaume Charmetant <cguille.dev@gmail.com>
2021-12-10 12:20:16 +0100

    Add day 10 part 2

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -1,7 +1,10 @@
+use std::fmt::{Display, Formatter};
+
 fn main() {
     let input = include_str!("input.txt");
 
     println!("{}", part1(input));
+    println!("{}", part2(input));
 }
 
 fn part1(input: &str) -> u64 {
@@ -34,6 +37,53 @@ fn part1(input: &str) -> u64 {
     illegal_score
 }
 
+fn part2(input: &str) -> u64 {
+    let mut stack;
+    let mut line_scores = Vec::new();
+
+    'line_loop: for line in input.lines() {
+        stack = Vec::new();
+
+        'symbol_loop: for symbol in line.chars().map(|c| Symbol::try_from(c).unwrap()) {
+            if symbol.is_opening() {
+                stack.push(symbol);
+                continue 'symbol_loop; // go to next symbol in the line
+            }
+
+            // here symbol is a closing one
+
+            let opening = stack.pop();
+
+            if opening.is_none() {
+                // we don't have any opening symbol for this closing symbol
+                continue 'line_loop; // go to next line in input
+            }
+
+            let opening = opening.unwrap();
+            if !opening.is_closed_by(&symbol) {
+                // corrupted: this closing symbol does not match its opening
+                continue 'line_loop; // go to next line in input
+            }
+        }
+
+        if stack.is_empty() {
+            // all opening symbols have had a matching closing symbol for this
+            // line, so there is nothing to complete
+            continue 'line_loop; // go to next line in input
+        }
+
+        line_scores.push(
+            stack
+                .into_iter()
+                .rfold(0, |acc, symbol| acc * 5 + symbol.autocomplete_score()),
+        );
+    }
+
+    line_scores.sort_unstable();
+
+    line_scores[line_scores.len() / 2]
+}
+
 #[derive(Debug)]
 struct ParseSymbolError;
 
@@ -56,13 +106,10 @@ impl Symbol {
             Self::Closing(_) => false,
             Self::Opening(opening) => match other {
                 Self::Opening(_) => false,
-                Self::Closing(closing) => match (opening, closing) {
-                    ('<', '>') => true,
-                    ('(', ')') => true,
-                    ('[', ']') => true,
-                    ('{', '}') => true,
-                    _ => false,
-                },
+                Self::Closing(closing) => matches!(
+                    (opening, closing),
+                    ('<', '>') | ('(', ')') | ('[', ']') | ('{', '}')
+                ),
             },
         }
     }
@@ -78,6 +125,32 @@ impl Symbol {
                 _ => 0,
             },
         }
+    }
+
+    fn autocomplete_score(&self) -> u64 {
+        match self {
+            Self::Closing(_) => 0,
+            Self::Opening(c) => match c {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => 0,
+            },
+        }
+    }
+}
+
+impl Display for Symbol {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Opening(c) => c,
+                Self::Closing(c) => c,
+            }
+        )
     }
 }
 
@@ -113,5 +186,10 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(part1(EXAMPLE), 26397);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(EXAMPLE), 288957);
     }
 }
